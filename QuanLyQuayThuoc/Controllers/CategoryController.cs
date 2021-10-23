8a9cf6ad504f8ced66801f8670594b911751ec20
NguyenThuongAnh <86449717+NguyenThuongAnh@users.noreply.github.com>
2021-10-23 14:54:04 +0700

    Initial commit

---
+++ QuanLyQuayThuoc/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using QuanLyQuayThuoc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyQuayThuoc.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        QuayThuocModelContext db = new QuayThuocModelContext();
+        public ActionResult ViewCategory(int id)
+        {
+            var listProduct = db.Products.Where(n => n.SmallCategory.category_id == id).ToList();
+            return View(listProduct);
+        }
+        public ActionResult ViewSmallCategory(int id)
+        {
+            var listProduct1 = db.Products.Where(n => n.smallcategory_id == id).ToList();
+            return View(listProduct1);
+        }
+    }
+}
